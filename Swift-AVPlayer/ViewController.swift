71af0f7c1bccfea6839c3e2d11e88993b8ea643c
品德信息 <pindexinxi@pindexinxidemac-mini.local>
2016-12-29 19:18:56 +0800

    Initial Commit

---
+++ Swift-AVPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-AVPlayer
+//
+//  Created by 品德信息 on 2016/12/29.
+//  Copyright © 2016年 品德信息. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
